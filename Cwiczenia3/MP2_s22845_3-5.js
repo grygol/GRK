e43922ffb3819f4f13843eb5edf10b4e2701802b
s22845 <s22845@pjwstk.edu.pl>
2022-11-27 21:21:59 +0100

    Zadanie 2 (3.5) skończone

--- Cwiczenia3/MP2_s22845_3-5.js
+++ Cwiczenia3/MP2_s22845_3-5.js
@@ -2,8 +2,6 @@ function setup(){
   console.log("Hello world");
   createCanvas(512, 512);
   background(255);
-
-  
 }
 
 var x0=-1;
@@ -30,22 +28,15 @@ ellipse(x1-3, y1-3, 6);
 function mouseReleased() {
 background(255);
 loadPixels();
-for(var i=0; i<512; i++){
-  x0=i;
-  y0=i;
-  x1=512;
-  y1=512;
-
-  draw_line();
-  updatePixels();
-}
+draw_line();
+updatePixels();
 
 }
 
 function set_pixel(x,y,c) {
 idx=(y * 512 + x) * 4;
-pixels[idx]= c<0 ? c : 0;
-pixels[idx+1]=0
+pixels[idx]= c * (-1);
+pixels[idx+1]= c
 pixels[idx+2]= 0;
 pixels[idx+3]=255;
 }
@@ -53,21 +44,15 @@ pixels[idx+3]=255;
 function draw_line() {
 var dx = x1 - x0;
 var dy = y1 - y0;
-var dx = 512;
-var dy = 512;
 var a = dy / dx;
 var b = y0 - a * x0;
 
-if(x1<x0){
-var tmp = x0;
-x0 = x1;
-x1 = tmp;
-}
-
-for(var x=x0; x<x1; x++){
-  var y = Math.round(a * x + b);
-  var d = ((dy/dx) * (x - x0)) - (y - y0);
-  set_pixel(x,y,d);
+for(y=0; y<512; y++){
+  for(x=0; x<512; x++){
+    var d = ((dy/dx) * (x - x0)) - (y - y0); //wariant 1
+    // var d = 2 * dy * (x-x0) - (2 * dx * (y - y0)); //wariant 2
+    set_pixel(x,y,d);
+  }
 }
 
 }
